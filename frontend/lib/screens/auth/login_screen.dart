2cfb51b5ccc2c3cafc309b4f310c1c8abd8509a7
CannonFodder151 <30951325+CannonFodder151@users.noreply.github.com>
2026-09-09 10:46:57 +1000

    fix: close children list in login_screen.dart Column

--- frontend/lib/screens/auth/login_screen.dart
+++ frontend/lib/screens/auth/login_screen.dart
@@ -219,7 +219,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           ),
                         ),
                       ),
-                    ),
+                      ],
                   ),
                   const SizedBox(height: 20),
                   const Text(
